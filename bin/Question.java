9b949f056ab9903ef7388165f870c34625b3e735
namratasingh99 <namsinghrajpoot9@gmail.com>
2026-07-11 15:30:02 +0530

    Add Question class with getters and setters

---
+++ bin/Question.java
@@ -0,0 +1,76 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class Question {
+   private int id;
+   private String ques;
+   private String opt1;
+   private String opt2;
+   private String opt3;
+   private String opt4;
+   private String ans;
+
+   public Question(int id, String ques, String opt1, String opt2, String opt3, String opt4, String ans) {
+      this.id = id;
+      this.ques = ques;
+      this.ans = ans;
+      this.opt1 = opt1;
+      this.opt2 = opt2;
+      this.opt3 = opt3;
+      this.opt4 = opt4;
+   }
+
+   public int getId() {
+      return this.id;
+   }
+
+   public void setId(int id) {
+      this.id = id;
+   }
+
+   public String getQues() {
+      return this.ques;
+   }
+
+   public void setQues(String ques) {
+      this.ques = ques;
+   }
+
+   public String getOpt1() {
+      return this.opt1;
+   }
+
+   public void setOpt1(String opt1) {
+      this.opt1 = opt1;
+   }
+
+   public String getOpt2() {
+      return this.opt2;
+   }
+
+   public void setOpt2(String opt2) {
+      this.opt2 = opt2;
+   }
+
+   public String getOpt3() {
+      return this.opt3;
+   }
+
+   public void setOpt3(String opt3) {
+      this.opt3 = opt3;
+   }
+
+   public String getOpt4() {
+      return this.opt4;
+   }
+
+   public void setOpt4(String opt4) {
+      this.opt4 = opt4;
+   }
+
+   public String getAns() {
+      return this.ans;
+   }
+
+   public void setAns(String ans) {
+      this.ans = ans;
+   }
+}
